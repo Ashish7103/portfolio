21d21beca77b95a469b4b32d4262ad26cbbc29c8
Ashish7103 <aksingh7103ashu@gmail.com>
2025-06-17 21:53:01 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Ashish";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["frontend development", "backend development", "web designing", "MernStack Developer", " fullStack development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
